4d35d17632eba52fcae26e1a3fceecee601df2b0
Mister Souza <mistersouza@icloud.com>
2024-10-23 00:50:06 +0200

    Set up mustache template engine

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 express = require('express');
+hogan = require('hogan-middleware')
+path = require('path');
+
 PORT = 3000;
 app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.engine('mustache', hogan.__express);
 
 app.get('/', (request, response, next) => (response.send('Hello World!')));
+app.get('/about', (request, response, next) => (response.json({ message: 'About page' })));
+
+app.get('/home', (request, response, next) => {
+    response.render('home', null);
+});
 app.listen(3000, () => console.log(`Server is running on http://localhost:${PORT}`));
